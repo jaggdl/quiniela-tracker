d42f8537368bdd752a16cdf356a71aae717acc28
Jose Alfredo <ja125garcia@gmail.com>
2026-06-17 19:43:27 -0600

    Allow setting is_admin cookie via URL param ?is_admin=1 and ?is_admin=0

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,5 +1,16 @@
 class ParticipantsController < ApplicationController
   def index
+    is_admin_param = params[:is_admin]
+    if is_admin_param
+      if is_admin_param == "1"
+        cookies[:is_admin] = { value: "1", expires: 1.year.from_now }
+      else
+        cookies.delete(:is_admin)
+      end
+      redirect_to root_path(sort: params[:sort].presence_in(%w[pts win]))
+      return
+    end
+
     @matches = Match.order(:matchday, :match_number)
     @show_win = cookies[:is_admin].present?
     @sort = params[:sort].presence_in(%w[pts win]) || "pts"
